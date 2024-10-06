a807c5aad2459cde085db6aad13a053fc20147a0
SahithiDande <103138939+SahithiDande@users.noreply.github.com>
2024-10-07 02:22:57 +0530

    Update config.js

--- src/config/env/config.js
+++ src/config/env/config.js
@@ -1,4 +1,6 @@
-export const BASE_URL = 'https://valamwebapp.herokuapp.com/';
+// export const BASE_URL = 'https://valamwebapp.herokuapp.com/';
+//export const BASE_URL = 'https://valamwebapp.herokuapp.com/';
+export const BASE_URL = 'https://valam-web-prod-ad6b0b1b95f7.herokuapp.com/';
 // export const GOOGLE_DIRECTIONS_API_KEY =
 //   'AIzaSyAGucoMHNFROxZFeUurLhxpSHNq-tpCUpo';
 export const GOOGLE_DIRECTIONS_API_KEY =
